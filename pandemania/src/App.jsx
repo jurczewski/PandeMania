377d6edbe09fb13350b8e4fe0d74e55e27ef3dda
Bartosz Jurczewski <bartek.jurczewski@gmail.com>
2021-01-26 00:40:31 +0100

    Basic api call

--- pandemania/src/App.jsx
+++ pandemania/src/App.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
+import Data from './components/DataFetcher/DataFetcher';
 
 function App() {
 	return (
 		<div className="App">
 			<h1>PandeMania</h1>
+			<Data countryName="Poland" />
 		</div>
 	);
 }
