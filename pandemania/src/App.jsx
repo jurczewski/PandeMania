066f0483a0dc888491265f9672af603a9f48a952
pawci0 <pgalewicz@gmail.com>
2021-02-11 20:12:16 +0100

    add routing and firebase config

--- pandemania/src/App.jsx
+++ pandemania/src/App.jsx
@@ -1,11 +1,30 @@
 import React from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import firebase from 'firebase';
 import Layout from './components/Layout/Layout';
+import Login from './components/Login/Login';
+import UserProvider from './context/user/UserProvider';
+import config from './FirebaseConfig';
+
+firebase.initializeApp(config);
 
 function App() {
+	console.log(firebase.auth().currentUser);
 	return (
-		<div className="App">
-			<Layout />
-		</div>
+		<UserProvider>
+			<div className="App">
+				<Router>
+					<Switch>
+						<Route path="/home">
+							<Layout />
+						</Route>
+						<Route path="/">
+							<Login />
+						</Route>
+					</Switch>
+				</Router>
+			</div>
+		</UserProvider>
 	);
 }
 
