968ec9ac1b1fb02ef6b7c557a395c7531fe46ca5
pawci0 <pgalewicz@gmail.com>
2021-02-15 18:59:21 +0100

    add firebase config

--- pandemania/src/FirebaseConfig.js
+++ pandemania/src/FirebaseConfig.js
@@ -1,10 +1,10 @@
 const config = {
-	apiKey: 'API_KEY',
-	authDomain: 'AUTH_DOMAIN',
-	projectId: 'PROJECT_ID',
-	storageBucket: 'STORAGE_BUCKET',
-	messagingSenderId: 'MESSAGING_ID',
-	appId: 'APP_ID',
+	apiKey: 'AIzaSyCGg--7lUP9qUJxPQUHmTyfkzZhW3AHsT8',
+	authDomain: 'pandemania-d9690.firebaseapp.com',
+	projectId: 'pandemania-d9690',
+	storageBucket: 'pandemania-d9690.appspot.com',
+	messagingSenderId: '404379003612',
+	appId: '1:404379003612:web:4a1c9b3218bfdf46d0a219',
 };
 
 export default config;
