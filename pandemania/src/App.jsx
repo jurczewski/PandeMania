3131d645e5b2398a3bafe7d50eb7f472b48571d2
Bartosz Jurczewski <bartek.jurczewski@gmail.com>
2021-02-20 19:21:38 +0100

    lazy loading

--- pandemania/src/App.jsx
+++ pandemania/src/App.jsx
@@ -1,33 +1,36 @@
-import React from 'react';
+import React, { Suspense } from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import UserProvider from './context/user/UserProvider';
-import RestrictedRoute from './components/RestrictedRoute/RestrictedRoute';
-import Layout from './components/Layout/Layout';
-import Login from './components/Login/Login';
-import Logout from './components/Logout/Logout';
 import { initialize } from './api/FirebaseAPI';
+
+const Login = React.lazy(() => import('./components/Login/Login'));
+const Layout = React.lazy(() => import('./components/Layout/Layout'));
+const Logout = React.lazy(() => import('./components/Logout/Logout'));
+const RestrictedRoute = React.lazy(() => import('./components/RestrictedRoute/RestrictedRoute'));
+const UserProvider = React.lazy(() => import('./context/user/UserProvider'));
 
 initialize();
 
 function App() {
 	return (
-		<UserProvider>
-			<div className="App">
-				<Router>
-					<Switch>
-						<Route path="/login">
-							<Login />
-						</Route>
-						<RestrictedRoute path="/logout">
-							<Logout />
-						</RestrictedRoute>
-						<Route path="/">
-							<Layout />
-						</Route>
-					</Switch>
-				</Router>
-			</div>
-		</UserProvider>
+		<Suspense fallback={<></>}>
+			<UserProvider>
+				<div className="App">
+					<Router>
+						<Switch>
+							<Route path="/login">
+								<Login />
+							</Route>
+							<RestrictedRoute path="/logout">
+								<Logout />
+							</RestrictedRoute>
+							<Route path="*">
+								<Layout />
+							</Route>
+						</Switch>
+					</Router>
+				</div>
+			</UserProvider>
+		</Suspense>
 	);
 }
 
