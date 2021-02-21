8c17c09bbbb8fe1f816bd7c81baa4497dafd59ca
shreq <shreq@users.noreply.github.com>
2021-02-21 19:32:48 +0000

    deploy: 704e3f200a94727cb6bc7e6584e02b0247f61e86

---
+++ static/js/13.b8e40b2c.chunk.js
@@ -0,0 +1,2 @@
+(this.webpackJsonppandemania=this.webpackJsonppandemania||[]).push([[13],{201:function(t,e,n){"use strict";n.r(e);var u=n(2),a=n(1),c=n(18),s=(n(65),n(4)),r=n(53);e.default=function(){var t=Object(a.useContext)(r.a);return Object(a.useEffect)((function(){t.fetched&&c.a.auth().signOut().then((function(){return t.setUser({user:null,fetched:!1})}))})),Object(u.jsx)(s.a,{to:"/"})}},53:function(t,e,n){"use strict";var u=n(1),a={user:null,fetched:!1,country:"",setUser:function(){}},c=n.n(u).a.createContext(a);e.a=c}}]);
+//# sourceMappingURL=13.b8e40b2c.chunk.js.map
